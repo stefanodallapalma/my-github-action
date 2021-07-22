a78898c5392448953cdfbb69a77c198c0326840b
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-07-22 20:23:21 +0200

    update

--- entrypoint.py
+++ entrypoint.py
@@ -9,6 +9,6 @@ language = os.getenv('INPUT_LANGUAGE')
 print('Model', model_id)
 print('Language', language)
 
-cmd='git diff --name-only ${{ github.event.before }} ${{ github.sha }}'
+cmd='git diff --name-only 27efb55d3576ec8844b274eec7527909758b2a6a 97d8f06cd2b35842fb90fea9e39a3cc734f6bda2'
 result=subprocess.getoutput(cmd)
 print("result::",result)
